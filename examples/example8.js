a282c32db35f7096f2387b452024ae8bea1f3828
= <=>
2026-01-21 15:59:58 +0530

    Datatypes and operators

---
+++ examples/example8.js
@@ -0,0 +1,9 @@
+let x = 10;
+if (x >= 10){
+    console.log("value of x is greater than or equal to 10");
+}
+else if( x === 10){
+    console.log("value of x is equal to 10");
+} else {
+    console.log("value of x is less than 10");
+}
---
+++ examples/example9.js
@@ -0,0 +1,11 @@
+let x = 10;
+switch (x) {
+    case 10:
+        console.log("value of x is 10");
+        break;
+        case 20:
+            console.log("value of x  is 20");
+            break;
+            default:
+                console.log("Unknown value");
+}
