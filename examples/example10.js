8c7acb04e1e1ef8b32a4397c5e9d233d2e431577
= <=>
2026-01-22 09:51:18 +0530

    second commit

---
+++ examples/example10.js
@@ -0,0 +1,2 @@
+let x = 10;
+x === 10 ? console.log('value is 10'): console.log('value is not 10');
---
+++ examples/example11.js
@@ -0,0 +1,6 @@
+let i;
+console.log(i);
+for ( i=110; false; i++){
+    console.log(i);
+}
+console.log(i);
